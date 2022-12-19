328dabe5447b36e6ca3c5af292f54ddedd76d7be
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-12-19 09:58:58 -0300

    Router

---
+++ src/pages/product/[id].tsx
@@ -0,0 +1,8 @@
+import { useRouter } from 'next/router'
+
+export default function Product() {
+    const { query } = useRouter()
+    return(
+        <h1>Olá Produto: {JSON.stringify(query)}</h1>
+    )
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,5 @@
+export default function Success() {
+    return (
+        <h1>Olá Success</h1>
+    )
+}
