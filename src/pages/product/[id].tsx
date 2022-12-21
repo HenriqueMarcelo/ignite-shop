da86769c729223a0fb453f02c1de153ac004a09c
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-12-21 09:42:50 -0300

    Product Page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,29 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
-  return <h1>Olá Produto: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 78,52</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eum
+          esse quaerat blanditiis assumenda recusandae asperiores? Corrupti
+          nesciunt quaerat molestiae excepturi, adipisci nisi voluptas
+          cupiditate impedit accusantium optio soluta eaque!
+        </p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
