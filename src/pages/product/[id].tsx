aa1776389868085c6d5014138822266012476a0d
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2022-12-23 16:35:51 -0300

    Prefetch only on hover

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -53,7 +53,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
 }) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
   const productId = params!.id
 
   const product = await stripe.products.retrieve(productId, {
