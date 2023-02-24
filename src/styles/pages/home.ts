84b93bd6d8f2b13029b152614004763ebe37e57a
Marcelo Henrique <marcelohenriquedasilvafonseca@gmail.com>
2023-02-24 16:38:37 -0300

    style: setting height for small screens

--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -6,6 +6,9 @@ export const HomeContainer = styled('main', {
   //   maxWidth: 'calc(100vw - ((100vw - 1180px)/2))',
   marginLeft: 'auto',
   minHeight: '656px',
+  '@media (max-height: 700px)': {
+    minHeight: '356px',
+  },
 })
 
 export const Product = styled('div', {
